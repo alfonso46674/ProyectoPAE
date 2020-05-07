bc8f5adca703119d9d5724aac887ec3d87a5bd49
alfonso46674 <49523948+alfonso46674@users.noreply.github.com>
2020-05-06 22:08:20 -0500

    all

--- BackEnd/routes/usuario.route.js
+++ BackEnd/routes/usuario.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const usuarioController = require('../controllers/usuario.controller.')
 
-router.get('/hola', usuarioController.Prueba)
+// router.get('/hola', usuarioController.Prueba)
 
 router.get('/', usuarioController.MostarUsuarios)
 
